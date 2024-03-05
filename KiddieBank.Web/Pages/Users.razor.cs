adb327a580d0293cf538cadb068c67c449fde073
Ernan <maisels@gmail.com>
2024-03-05 16:00:24 +0200

    seperate html from code

---
+++ KiddieBank.Web/Pages/Users.razor.cs
@@ -0,0 +1,14 @@
+﻿using KiddieBank.Model.Models;
+
+namespace KiddieBank.Web.Pages
+{
+    public partial class Users
+    {
+        private List<User> _users { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _users = (await _userService.GetUsersAsync()).ToList();
+        }
+    }
+}
