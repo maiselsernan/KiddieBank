224ed47439b14c73096628de42622a8b74f9eed9
Ernan <maisels@gmail.com>
2024-03-12 19:42:40 +0200

    add migration

---
+++ KiddieBank.Api/Migrations/20240312144633_UserPasswordAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KiddieBank.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "VARBINARY(MAX)",
+                nullable: true);
+           
+            migrationBuilder.AddColumn<byte[]>(
+               name: "PasswordSalt",
+               table: "Users",
+               type: "VARBINARY(MAX)",
+               nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+           name: "PasswordHash",
+           table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
--- KiddieBank.Model/Models/User.cs
+++ KiddieBank.Model/Models/User.cs
@@ -10,6 +10,7 @@ public partial class User
     public string Name { get; set; } = null!;
 
     public int Role { get; set; }
-
+    public byte[] PasswordHash { get; set; }   
+    public byte[] PasswordSalt { get; set; }   
     public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
 }
